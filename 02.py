2aaee6f0f1d8268cc0847124274344452b9f0e89
shinespark <shinespark64@gmail.com>
2015-07-23 16:07:59 +0900

    add until 07.py

---
+++ 02.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+
+str1 = 'パトカー'
+str2 = 'タクシー'
+
+length = len(str1)
+
+for i in range(length):
+    print(str1[i], end='')
+    print(str2[i], end='')
---
+++ 07.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+
+
+def template(x, y, z):
+    return str(x) + '時の' + str(y) + 'は' + str(z)
+
+print(template('あ', 'い', 3))
+print(template(12, '気温', 22.4))
